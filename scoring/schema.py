7fede2c4291e97d1632eaa346e23953a473452c5
ToniKousek <toni.kousek@gmail.com>
2024-05-05 21:54:35 +0200

    feat: add python schema for competitions

---
+++ scoring/schema.py
@@ -0,0 +1,78 @@
+"""
+When creating a Competition:
+    Create Competitor objects, and add them to a list
+    Create Field objects, and add them to a list
+    Create the Competition with the two lists
+
+On a new scorer:
+    Get the name
+    Call Competition.add_scorer(scorer_name)
+
+"""
+
+
+class Field:
+    def __init__(self, name: str, max_value: int) -> None:
+        self.name = name
+        self.max_value = max_value
+
+    def __eq__(self, other: object) -> bool:
+        if not hasattr(other, "name"):
+            return False
+        return other.name == self.name  # type: ignore
+
+
+class Competitor:
+    def __init__(self, name: str, index: int) -> None:
+        self.name = name
+        self.index = index
+        # dict[scorer_name, dict[Field, value]]
+        self.scores: dict[str, dict[Field, int | None]] = {}
+
+    def score(self, scorer_name: str, field: Field, score: int):
+        self.scores[scorer_name][field] = score
+
+
+class Competition:
+    def __init__(
+            self,
+            fields: list[Field],
+            competitors: list[Competitor],
+            scorers: list[str] | None = None,
+    ) -> None:
+
+        self.fields = fields
+        self.competitors = competitors
+        self.scorers = scorers if scorers else []
+        self.current_competitor_index = 0
+
+    def add_scorer(self, name: str):
+        self.scorers.append(name)
+        for competitor in self.competitors:
+            competitor.scores[name] = {field: None for field in self.fields}
+
+    def score(self, scorer_name: str, competitor_index: int, field: Field, value: int):
+        self.competitors[competitor_index].score(scorer_name, field, value)
+
+
+competition: Competition | None = None
+
+
+def get_competition() -> Competition | None:
+    global competition
+    return competition
+
+
+def new_competition(
+    fields: list[Field],
+    competitors: list[Competitor],
+    scorers: list[str] | None = None,
+) -> Competition:
+    global competition
+    competition = Competition(fields, competitors, scorers)
+    return competition
+
+
+def delete_competition():
+    global competition
+    competition = None
